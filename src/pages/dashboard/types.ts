9d48d97df032473d6718defa57948aa8053774db
Joshua Liu <mathmanjosh@gmail.com>
2022-09-11 14:45:07 -0400

    types fix

--- src/pages/dashboard/types.ts
+++ src/pages/dashboard/types.ts
@@ -1,4 +1,4 @@
-type cardState = { id: number; text: string; color: string }
+type cardState = { id: number; text: string; color?: string, time?: string }
 type colState = { id: number; title: string; color: string, items: cardState[] }
 
 export type { cardState, colState };
