89d85d67c0fa395a573b14581942b5c8646d126f
Joshua Liu <supajoshjunk@gmail.com>
2022-09-12 17:06:41 +0000

    load screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from './pages/home';
+import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Dashboard from "./pages/dashboard";
+import { useEffectOnce } from "react-use";
 
 function App() {
   const [cards, setCards] = useState([
@@ -14,17 +15,21 @@ function App() {
     },
   ]);
 
+  useEffectOnce(() => {
+    document.getElementById("splash").classList.add("hidden");
+  });
+
   return (
     <div className="app">
       <Router>
         <Routes>
-					<Route exact path="/" element={<Home/>}/>
-          <Route exact path="/login" element={<Login/>} />
-					<Route exact path="/register" element={<Register/>} />
-					<Route exact path="/dashboard" element={<Dashboard/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
 
-					{/* 404 page */}
-					<Route path="*" element={<div>404</div>} />
+          {/* 404 page */}
+          <Route path="*" element={<div>404</div>} />
         </Routes>
       </Router>
     </div>
