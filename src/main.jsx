56e065566b11d9c656fbe7276dd79b17cbb4bea0
Joshua Liu <mathmanjosh@gmail.com>
2022-09-17 10:27:04 -0400

    preconnect and remove eruda in build

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import "./index.css";
 const render = () =>
   ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-// if (import.meta.env.DEV) {
-if (true) {
+if (import.meta.env.DEV) {
+// if (true) {
   const script = document.createElement("script");
   script.src = "/eruda.js";
   document.body.appendChild(script);
