aa4575a7d3ab3eaa64df14a453947800d8d40c41
Joshua Liu <supajoshjunk@gmail.com>
2022-10-14 16:27:14 +0000

    some eruda stuff

--- public/loadEruda.js
+++ public/loadEruda.js
@@ -1,19 +1,34 @@
-try {
-  eruda.init();
-  if (localStorage.getItem("eruda-auto-show") === "1") eruda.show();
-  window.addEventListener("keydown", (e) => {
-    const { key, ctrlKey } = e;
-    if (key === "e" && ctrlKey) {
-      e.preventDefault();
-      if (localStorage.getItem("eruda-auto-show") === "1") {
-        localStorage.setItem("eruda-auto-show", "0");
-        eruda.hide();
-      } else {
-        localStorage.setItem("eruda-auto-show", "1");
-        eruda.show();
+const erudaScript = document.createElement("script");
+erudaScript.addEventListener("load", () => {
+  try {
+    eruda.init({
+      defaults: {
+        displaySize: 30,
+        theme: "Atom One Dark",
+        transparency: 1,
+      },
+    });
+    if (localStorage.getItem("eruda-auto-show") === "1") eruda.show();
+    window.addEventListener("keydown", (e) => {
+      const { key, ctrlKey } = e;
+      if (key === "e" && ctrlKey) {
+        e.preventDefault();
+        if (localStorage.getItem("eruda-auto-show") === "1") {
+          localStorage.setItem("eruda-auto-show", "0");
+          eruda.hide();
+        } else {
+          localStorage.setItem("eruda-auto-show", "1");
+          eruda.show();
+        }
       }
-    }
-  });
-} catch (e) {
-  console.error(e);
-}
+    });
+  } catch (e) {
+    alert(e);
+  }
+});
+
+erudaScript.addEventListener("error", (e) => alert(e.message));
+
+erudaScript.src = "https://cdn.jsdelivr.net/npm/eruda@2.5.0/eruda.js";
+
+document.body.appendChild(erudaScript);
